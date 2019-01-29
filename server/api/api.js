5f58e107c1d9ccd771582374ef9df2ecabfe913c
Michael Lin <mlin4589@gmail.com>
2019-01-28 20:44:08 -0500

    Response is now JSON

--- server/api/api.js
+++ server/api/api.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 8888
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/test', (req, res) => res.send('Hello World!'))
+app.post('/test', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({success:true}))
+  }
+)
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
