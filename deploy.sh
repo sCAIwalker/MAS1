3529ab28a99bfc4a991a8e8ebe486a7558650e37
Michael Lin <mlin4589@gmail.com>
2019-01-28 23:13:05 -0500

    Added Mongodb

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -r . ubuntu@3.93.95.228:~/app
+rsync -r . ubuntu@3.93.95.228:~/app --delete
 ssh ubuntu@amazon 'cd ~/app/server && sudo docker-compose down && sudo docker-compose up -d'
--- server/api/api.js
+++ server/api/api.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 8888
+const bodyParser = require('body-parser');
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect("mongodb://db:27017/test");
+var nameSchema = new mongoose.Schema({
+ Name: String,
+});
+var User = mongoose.model("User", nameSchema);
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/test', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({success:true}))
-  }
-)
+  var myData = new User(req.body);
+  myData.save()
+    .then(item => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({success:true}))
+      console.log("Created User!");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
